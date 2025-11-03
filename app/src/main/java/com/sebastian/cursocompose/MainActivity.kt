07a387c8ecacf9f65458497600c3ba7fd782d470
sebastian <slvalenzu@gmail.com>
2025-11-03 19:35:52 -0300

    Crear un divisor entre dos textos por ejemplo

--- app/src/main/java/com/sebastian/cursocompose/MainActivity.kt
+++ app/src/main/java/com/sebastian/cursocompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sebastian.cursocompose
 
+import android.R
 import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,11 +9,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.ProgressIndicatorDefaults
@@ -26,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,67 +44,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-             Login()
+          Column(modifier = Modifier.fillMaxSize(),
+              verticalArrangement = Arrangement.Center,
+          horizontalAlignment = Alignment.CenterHorizontally) {
+              Text(text= "Hola!")
+              Divider(color = Color.Black,
+                 // modifier = Modifier.fillMaxHeight().width(10.dp))
+                  modifier = Modifier.fillMaxWidth().height(10.dp))
+              Text(text = "Mundo!")
+          }
         }
-    }
-}
-
-@Composable
-fun Login(){
-    Column(modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center){
-        var username by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-        var res by remember { mutableStateOf("No hay resultado") }
-
-        OutlinedTextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Usuario") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
-            singleLine = true
-        )
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Clave") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
-            singleLine = true,
-            visualTransformation = PasswordVisualTransformation()
-        )
-        Button(onClick = {
-            var cadena = ""
-            if(username.isEmpty()){
-                cadena = "No puedes dejar el usuario vacio"
-            }else if(password.isEmpty()){
-                cadena = "No puedes dejar la clave vacia"
-            }
-            res = cadena
-        }, modifier = Modifier
-            .padding(10.dp).align(Alignment.CenterHorizontally)) {
-            Text(text = "Iniciar Sesión")
-        }
-        Text(text = res, modifier = Modifier.fillMaxWidth())
-        LinearProgressIndicator(
-        progress = { 0.55f },
-        modifier = Modifier.fillMaxWidth()
-                    .padding(10.dp),
-        color = ProgressIndicatorDefaults.linearColor,
-        trackColor = ProgressIndicatorDefaults.linearTrackColor,
-        strokeCap = ProgressIndicatorDefaults.LinearStrokeCap,
-        )
-        CircularProgressIndicator(
-        progress = { 0.9f },
-        modifier = Modifier.fillMaxWidth().padding(horizontal = 150.dp, vertical = 10.dp),
-        color = ProgressIndicatorDefaults.circularColor,
-        strokeWidth = ProgressIndicatorDefaults.CircularStrokeWidth,
-        trackColor = ProgressIndicatorDefaults.circularIndeterminateTrackColor,
-        strokeCap = ProgressIndicatorDefaults.CircularDeterminateStrokeCap,
-        )
-
     }
 }
