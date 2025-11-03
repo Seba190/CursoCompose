620a44868915abf98bbc6572f7a205d3abfea5f9
sebastian <slvalenzu@gmail.com>
2025-11-03 16:50:22 -0300

    Crear un login básico con jetpack compose

--- app/src/main/java/com/sebastian/cursocompose/MainActivity.kt
+++ app/src/main/java/com/sebastian/cursocompose/MainActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,34 +35,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CursoComposeTheme {
-                Box(modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center){
-                    SelectorNumerico()
-                }
-                }
-
+             Login()
         }
     }
 }
 
 @Composable
-fun SelectorNumerico(){
-    var valorActual by remember{ mutableIntStateOf(0) }
-    BotonesSelector(valorActual=valorActual, presiona = {
-        valorActual += it
-    })
-}
+fun Login(){
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center){
+        var username by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var res by remember { mutableStateOf("No hay resultado") }
 
-@Composable
-fun BotonesSelector(valorActual: Int, presiona: (Int) -> Unit){
-    Column {
-        Button(onClick = {presiona(-1)}) {
-            Text(text = "Restar!")
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Usuario") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Clave") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Button(onClick = {
+            var cadena = ""
+            if(username.isEmpty()){
+                cadena = "No puedes dejar el usuario vacio"
+            }else if(password.isEmpty()){
+                cadena = "No puedes dejar la clave vacia"
+            }
+            res = cadena
+        }, modifier = Modifier
+            .padding(10.dp).align(Alignment.CenterHorizontally)) {
+            Text(text = "Iniciar Sesión")
         }
-        Text(text = "$valorActual", modifier = Modifier.padding(start = 35.dp),fontSize = 30.sp)
-        Button(onClick = {presiona(+1)}) {
-            Text(text = "Sumar!")
-        }
+        Text(text = res, modifier = Modifier.fillMaxWidth())
+
     }
 }
