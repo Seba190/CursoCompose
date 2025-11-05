ab7eac46d215ac68939420a02420e225ec3c840f
Sebastián Alexis Valenzuela Henríquez <sebastianalexisvalenzuelahenriquez@MacBook-Air-de-Sebastian.local>
2025-11-05 20:57:09 -0300

    Creo animaciones usando Dp as State, esta vez el logo de android

--- app/src/main/java/com/sebastian/cursocompose/MainActivity.kt
+++ app/src/main/java/com/sebastian/cursocompose/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -48,15 +50,19 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sebastian.cursocompose.ui.theme.CursoComposeTheme
@@ -67,8 +73,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-         AnimableEjemplo()
+             AnimarDpAsState()
         }
+    }
+    @Composable
+    fun ImagenCircular(imageSize: Dp){
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "imagen ciruclar",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(imageSize)
+                .clip(CircleShape)
+                .border(5.dp, Color.LightGray, CircleShape)
+        )
+    }
+    @Composable
+    fun AnimarDpAsState(){
+       val isNeedExpansion = rememberSaveable {mutableStateOf(false) }
+        val animatedSizeDp: Dp by animateDpAsState(targetValue = if(isNeedExpansion.value) 350.dp else 100.dp)
+       Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
+           ImagenCircular(imageSize = animatedSizeDp)
+           Button(onClick = {isNeedExpansion.value = !isNeedExpansion.value},
+               modifier = Modifier.padding(top = 50.dp)
+                   .width(300.dp)) {
+               Text(text = "Animate DP as State!")
+           }
+       }
+
     }
 
 @Composable
